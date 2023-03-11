a957bbeed72d27e64a53cfd91ac9fc15baa70018
mahmoud hassan <abosamour990@gmail.com>
2023-03-11 22:22:27 +0200

    add task mahmoud_abuelhasaan

--- src/App.js
+++ src/App.js
@@ -5,12 +5,7 @@ import RegisterFreelancer from './Components/index/registerFreelancer';
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <RegisterFreelancer/>
-      </BrowserRouter>
-      
-
-
+      <RegisterFreelancer/>
     </div>
     
   );
