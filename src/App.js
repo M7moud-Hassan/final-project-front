a7c4ee534024beb7f19a6a9b23865a6057f47315
Ahmed Abdelnasser <ahmed.abdelnasser7777777@gmail.com>
2023-03-11 16:25:41 +0200

    Login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Components/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+    constructor(){
+        super();
+        this.state = {
+
+        }
+    }
+    render(){
+        return (
+                <BrowserRouter>
+                    {/* <Header/>            */}
+                        <Routes>
+                            <Route path='/login' element={<Login/>}/>
+                            {/* <Route path='/*' element={<Error/>}/> */}
+                        </Routes>
+                    {/* <Footer/>  */}
+                </BrowserRouter>
+            )
+        }
 }
-
 export default App;
