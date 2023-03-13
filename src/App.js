1903c5350f78f894a4187dfd45356252b7b646c0
Mahmoud Abuelhasaan Abdelhamed <mahmoud.abuelhasaan@fci.luxor.edu.eg>
2023-03-13 10:32:01 +0200

    connect between api and front end (regester(user,freelancer)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import { BrowserRouter } from 'react-router-dom';
-import RegisterFreelancer from './Components/index/registerFreelancer';
+// import RegisterFreelancer from './Components/index/registerFreelancer';
+import RegisterUser from './Components/index/registerUser';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <RegisterFreelancer/>
+        {/* <RegisterFreelancer/> */}
+        <RegisterUser/>
       </BrowserRouter>
       
 
