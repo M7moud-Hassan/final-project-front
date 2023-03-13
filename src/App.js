92d3562613a78b62a3779c4920e32e2b0e958a58
mahmoud hassan <abosamour990@gmail.com>
2023-03-13 19:03:32 +0200

    merge rania

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './Components/home_page';
 import ActivateFreeLancer from './Components/index/activate';
 import AddDetails from './Components/index/add_details';
 import Login from './Components/index/Login';
+import Nxsignup from './Components/index/Nxsignup';
 import RegisterFreelancer from './Components/index/registerFreelancer';
 
 
@@ -16,6 +17,9 @@ function App() {
       <Route path='/activate_freelancer/:uid/:token' element={<ActivateFreeLancer/>}/>
       <Route path='/addDetails' element={<AddDetails/>}/>
       <Route path='/registration_fresslancer' element={<RegisterFreelancer/>}/>
+      <Route path='/choose_account' element={<Nxsignup/>}/>
+      <Route path='/registration_user' element={<RegisterFreelancer/>}/>
+      <Route path='/addDetails' element={<AddDetails/>}/>
       </Routes>
       </BrowserRouter>
   )
