85bd8c029f8bbd6764b0330822b182872d2e7bde
mahmoud hassan <abosamour990@gmail.com>
2023-03-13 11:22:33 +0200

    add details

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Components/home_page';
 import ActivateFreeLancer from './Components/index/activate';
+import AddDetails from './Components/index/add_details';
 import Login from './Components/index/Login';
 
 
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<Home/>}/>
       <Route path="/login" element={<Login/>}/>
       <Route path='/activate_freelancer/:uid/:token' element={<ActivateFreeLancer/>}/>
+      <Route path='/addDetails' element={<AddDetails/>}/>
       </Routes>
       </BrowserRouter>
   )
