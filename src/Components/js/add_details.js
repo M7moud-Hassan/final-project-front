1ee8aa3cb0f4af998d72a077c578409e294b7091
mahmoud hassan <abosamour990@gmail.com>
2023-03-16 13:58:25 +0200

    merge tasks

--- src/Components/js/add_details.js
+++ src/Components/js/add_details.js
@@ -1,12 +1,12 @@
 
 
 
-
+import  './choices';
 var script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-3.6.3.min.js';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-import  './choices';
+
 
 
 $( document ).ready(function() {
