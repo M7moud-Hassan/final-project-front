3a16f1fd6e0eda47f7c399fe04ce3cd2ea219545
Ahmed Abdelnasser <ahmed.abdelnasser7777777@gmail.com>
2023-03-20 17:17:26 +0200

    Freelancer Setting page

--- src/Components/profile_free.js
+++ src/Components/profile_free.js
@@ -1,20 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import FreeSettings from "./Profile/freelancer/FreelancerSettings";
+import FreeSettings1 from "./Profile/freelancer/FreelancerSettings";
 import PortfolioAddDetails from "./Profile/freelancer/PortfolioAddDetails";
 import PortfolioAddProject from "./Profile/freelancer/PortfolioAddProject";
 import Profile from "./Profile/freelancer/profile"
 
 let ProfileFreeLancer = () => {
 
-    if(localStorage.getItem('uid'))
-    {
-      if(localStorage.getItem('type')=='free'){
-        return <Profile/>
-    }
-    else{
-        window.location='/clientprofile'
-    }
-    }else{
-    window.location='/error'
-    }  
+    // if (localStorage.getItem('uid')) {
+    //     if (localStorage.getItem('type') == 'free') {
+    //         return <Profile />
+    //         // return <FreeSettings/>
 
+    //     }
+    //     else {
+    //         window.location = '/clientprofile'
+    //     }
+    // } else {
+    //     window.location = '/error'
+    // }
+    return <FreeSettings/>
 }
 export default ProfileFreeLancer;
