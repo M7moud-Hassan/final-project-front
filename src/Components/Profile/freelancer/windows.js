422fbe5b3da7a5beedd8e3a4dd45b812c98b6023
mahmoud hassan <abosamour990@gmail.com>
2023-03-27 13:05:35 +0200

    merge tasks

--- src/Components/Profile/freelancer/windows.js
+++ src/Components/Profile/freelancer/windows.js
@@ -2,10 +2,13 @@
 window.onscroll=function(){
     const DoM = document.getElementById("setting")
         DoM.style.display = 'none'
+    var box  = document.getElementById('box');
+    box.style.height  = '0px';
+    box.style.opacity = 0;
 }
 window.onclick=function(e){
     if(e.target!=document.getElementById("m7moud")){
     const DoM = document.getElementById("setting")
     DoM.style.display = 'none'
-    }
+    } 
 }
