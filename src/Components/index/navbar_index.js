fe4eb9c9048d79e2e90a83bcb697dba72544a984
Ahmed Abdelnasser <ahmed.abdelnasser7777777@gmail.com>
2023-03-29 01:34:54 +0200

    fixes

--- src/Components/index/navbar_index.js
+++ src/Components/index/navbar_index.js
@@ -11,16 +11,16 @@ class NavBarIndex extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary ">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary navbarReducePadding ">
 
           <div className='container-fluid'>
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-              </button>
-              <NavLink to={'/'}><img id='logo' className='ms-5' src='\images\inLogo.png' /></NavLink>
-          <div class="collapse navbar-collapse" id="navbarScroll">
-          <div className='d-flex'>
-          <ul className='mt-3'>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <NavLink to={'/'}><img id='logo' className='ms-3' src='\images\inLogo.png' /></NavLink>
+            <div class="collapse navbar-collapse" id="navbarScroll">
+              <div className='d-flex'>
+                <ul className='mt-3'>
                   <li className="nav-item dropdown dropFont">
                     <a className="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                       Help & About Center
@@ -32,27 +32,29 @@ class NavBarIndex extends Component {
                     </div>
                   </li>
                 </ul>
-          
-          </div>
-          <div className='w-100' >
-          <form className="d-flex">
-            <div className="input-group ms-5 w-100">
-              <div className="search_box d-flex w-50">
-               
+
               </div>
-              <NavLink className={'btn border-0 rounded ms-5' } to={"/login"}>Log In</NavLink>
-              <NavLink className="btn btn-success border-0 rounded"  to={"/choose_account"}>Sign Up</NavLink>
+              <div className='w-100' >
+                <form className="d-flex">
+                  <div className="input-group ms-5 w-100">
+                    <div className="search_box d-flex w-50">
+
+                    </div>
+                    <div className='w-50 text-end '>
+                    <NavLink className={'btn border-0 rounded me-3'} to={"/login"}>Log In</NavLink>
+                    <NavLink className="btn btn-success border-0 rounded me-5" to={"/choose_account"}>Sign Up</NavLink>
+                    </div>
+                  </div>
+
+                </form>
+              </div>
             </div>
-            
-          </form>
-          </div>
-          </div>
           </div>
         </nav>
-        
-        
+
+
       </div>
-      
+
     )
   }
 }
--- src/Components/index/registerUser.js
+++ src/Components/index/registerUser.js
@@ -5,10 +5,10 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 
 class RegisterUser extends Component {
-  
+
   constructor(props) {
     super(props);
-   
+
     this.state = {
       firstName: '',
       lastName: '',
@@ -82,191 +82,197 @@ class RegisterUser extends Component {
 
 
   render() {
-    var type= localStorage.getItem("type");
-    if(type=='free'){
-    window.location = '/home_freelancer'
-    }else if(type=='client')
-    {
-        window.location = '/clientprofile'
-    }else{
-    return (
-      <div>
+    var type = localStorage.getItem("type");
+    if (type == 'free') {
+      window.location = '/home_freelancer'
+    } else if (type == 'client') {
+      window.location = '/clientprofile'
+    } else {
+      return (
+        <div>
 
-        <nav className="navbar navbar-expand-lg navbar-light bg-white">
-          <div className="container">
-          <NavLink onClick={
-                        ()=>{
-                            window.location='/'
-                        }
-                    }><img id='logo' className='ms-5' src='\images\upwork.png' /></NavLink>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
+          <nav className="navbar navbar-expand-lg navbar-light bg-white">
+            <div className="container">
+              <NavLink className="" onClick={
+                () => {
+                  window.location = '/'
+                }
+              }><img className='imgLogo' src="images/inLogo.png " alt="Logo"></img></NavLink>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNav">
 
-              <div className="navbar-nav ms-auto">
-                <div className="text-center fontEditsHalf">
-                  Looking for work?
-                  <NavLink className={'text-center text-success mt-3 fontEditsHalf' } onClick={
-                    ()=>{
-                      window.location='/registration_freelancer'
-                    }
-                  }>Applay as Talent</NavLink>
+                <div className="navbar-nav ms-auto">
+                  <div className="text-center">
+
+
+                    <NavLink className={'text-center text-success mt-3 fontEditsHalf ms-3'} onClick={
+                      () => {
+                        window.location='/registration_freelancer'
+                      }
+                    }><button className='btn btn-success '>Sign up as a Freelancer</button></NavLink>
+                    <span className='ms-3 BBold'>or</span>
+                                        <NavLink className={'text-center text-success mt-3 fontEditsHalf ms-3'} onClick={
+                      () => {
+                        window.location='/login'
+                      }
+                    }><button className='btn  btn-outline-success'>Login</button></NavLink>
+                  </div>
                 </div>
+
+
               </div>
-
-
             </div>
-          </div>
-        </nav>
+          </nav>
 
 
-        <div className="container">
+          <div className="container">
 
-          <section className="vh-100 gradient-custom">
-            <div className="container py-5 h-100">
-              <div className="row justify-content-center align-items-center h-100">
-                <div className="col-12 col-lg-9 col-xl-7">
-                  <div className="card shadow-2-strong card-registration my-5" id='register_form_card'>
-                    <div className="card-body p-4 p-md-5">
-                      <h3 className="mb-4 pb-2 pb-md-0 mb-md-5 text-center">Sign up to hire talent</h3>
-
-
-                      <form method='post' onSubmit={this.handleSubmit} className="needs-validation" noValidate>
+            <section className="vh-100 gradient-custom">
+              <div className="container py-5 h-100">
+                <div className="row justify-content-center align-items-center h-100">
+                  <div className="col-12 col-lg-9 col-xl-7">
+                    <div className="card shadow-2-strong card-registration my-5" id='register_form_card'>
+                      <div className="card-body p-4 p-md-5">
+                        <h3 className="mb-4 pb-2 pb-md-0 mb-md-5 text-center">Sign up to hire talent</h3>
 
 
-                        <div className="row">
-                          <div className="col-md-6 mb-4">
-                            <div className="form-outline position-relative">
-                              <input type="text" id="firstName" placeholder="First Name" required
-                                className="form-control  rounded-pill" value={this.state.firstName} onChange={this.handleFirstNameChange} />
+                        <form method='post' onSubmit={this.handleSubmit} className="needs-validation" noValidate>
+
+
+                          <div className="row">
+                            <div className="col-md-6 mb-4">
+                              <div className="form-outline position-relative">
+                                <input type="text" id="firstName" placeholder="First Name" required
+                                  className="form-control  rounded-pill" value={this.state.firstName} onChange={this.handleFirstNameChange} />
+                                <div className="invalid-feedback text-center">
+                                  First name is required
+                                </div>
+                              </div>
+                            </div>
+
+                            <div className="col-md-6 mb-4">
+                              <div className="form-outline position-relative">
+                                <input type="text" id="lastName" placeholder="Last Name" required
+                                  className="form-control  rounded-pill ms-md-3" value={this.state.lastName} onChange={this.handleLastNameChange} />
+                                <div className="invalid-feedback text-center">
+                                  Last name is required
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+
+                          <div className="row">
+                            <div className="col-md-12 mb-1">
+                              <div className="form-outline position-relative">
+                                <input type="email" id="email" placeholder="Work email adress" required
+                                  pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}"
+                                  className="form-control  rounded-pill" value={this.state.email} onChange={this.handleEmailChange} onBlur={
+                                    () => {
+                                      axios.post('http://127.0.0.1:8000/auth/check_email/', { "email": this.state.email }).then((response) => {
+                                        if (response.data == 'ok') {
+                                          this.setState({ email_exists: true })
+                                        } else {
+                                          this.setState({ email_exists: false })
+                                        }
+                                      })
+                                    }
+                                  } />
+                                {this.errorEmai()}
+                                <div className="invalid-feedback text-center"
+                                  id="email-feedback">
+                                  Email is required
+
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+
+                          <div className="row">
+                            <div className="col-md-12 mb-4">
+                              <div className="form-outline position-relative">
+                                <input type="password" name="password" id="password"
+                                  placeholder="Password(8 or more characters)" pattern="(?=.*\d)(?=.*[a-zA-Z]).{8,}" required
+                                  className="form-control  rounded-pill" value={this.state.password} onChange={this.handlePasswordChange} />
+                                <div className="invalid-feedback text-center" id="password-feedback">
+                                  Password must be at least 8 characters long and contain at least one letter and one digit
+                                </div>
+                                <div className="invalid-feedback text-center" id="password-required-feedback">
+                                  Please enter a password
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+
+
+
+                          <div className="form-group">
+                            <div className="input-group position-relative">
+                              <input type="text" className="form-control rounded-pill" placeholder="Phone number" id="phone"
+                                name="phone" pattern="01[0-2]{1}[0-9]{8}" required value={this.state.phone} onChange={this.handlePhoneChange}></input>
                               <div className="invalid-feedback text-center">
-                                First name is required
+                                Please enter your Egyption phone like "01*********"
+                              </div>
+
+                            </div>
+                          </div>
+
+                          <div className="form-check mt-4">
+                            <input className="form-check-input" type="checkbox" id="send_emails" name="send_emails"></input>
+                            <label className="form-check-label fixedFonts" htmlFor="send_emails">
+                              Send me helpful emails to find rewarding work and job leads
+                            </label>
+                          </div>
+
+                          <div className=" row form-check mt-2">
+                            <div className="col-md-12 mb-4">
+                              <div className="form-outline position-relative">
+                                <input className="form-check-input" type="checkbox" id="agree_terms" name="agree_terms" required></input>
+                                <label className="form-check-label fixedFonts" htmlFor="agree_terms">
+                                  Yes, I understand and agree to the Upwork Terms of Service, including the <a href="#" className="text-success">User
+                                    Agreement</a> and <a href="#" className="text-success">Privacy Policy</a>
+                                </label>
+                                <div className="invalid-feedback text-danger" id="email-feedback">
+                                  Please accept the Upwork Terms of Service before continuing
+                                </div>
                               </div>
                             </div>
                           </div>
 
-                          <div className="col-md-6 mb-4">
-                            <div className="form-outline position-relative">
-                              <input type="text" id="lastName" placeholder="Last Name" required
-                                className="form-control  rounded-pill ms-md-3" value={this.state.lastName} onChange={this.handleLastNameChange} />
-                              <div className="invalid-feedback text-center">
-                                Last name is required
-                              </div>
-                            </div>
+
+                          <div className="mt-4 pt-2">
+                            <input className="btn btn-success btn-block rounded-pill m-3 w-100" type="submit" value="Submit" />
                           </div>
-                        </div>
 
-                        <div className="row">
-                          <div className="col-md-12 mb-1">
-                            <div className="form-outline position-relative">
-                              <input type="email" id="email" placeholder="Work email adress" required
-                                pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}"
-                                className="form-control  rounded-pill" value={this.state.email} onChange={this.handleEmailChange} onBlur={
-                                  () => {
-                                    axios.post('http://127.0.0.1:8000/auth/check_email/', { "email": this.state.email }).then((response) => {
-                                      if (response.data == 'ok') {
-                                        this.setState({ email_exists: true })
-                                      } else {
-                                        this.setState({ email_exists: false })
-                                      }
-                                    })
-                                  }
-                                } />
-                              {this.errorEmai()}
-                              <div className="invalid-feedback text-center"
-                                id="email-feedback">
-                                Email is required
-
-                              </div>
-                            </div>
+                          <div className="text-center">
+                            Already have an account?
+                            <NavLink to={'/login'} className="text-center text-success mt-3"> Login</NavLink>
                           </div>
-                        </div>
+                        </form>
+                        <div className="my-3">
 
-                        <div className="row">
-                          <div className="col-md-12 mb-4">
-                            <div className="form-outline position-relative">
-                              <input type="password" name="password" id="password"
-                                placeholder="Password(8 or more characters)" pattern="(?=.*\d)(?=.*[a-zA-Z]).{8,}" required
-                                className="form-control  rounded-pill" value={this.state.password} onChange={this.handlePasswordChange} />
-                              <div className="invalid-feedback text-center" id="password-feedback">
-                                Password must be at least 8 characters long and contain at least one letter and one digit
-                              </div>
-                              <div className="invalid-feedback text-center" id="password-required-feedback">
-                                Please enter a password
-                              </div>
-                            </div>
-                          </div>
-                        </div>
+                          <div>
+                            <button className="btn btn-primary btn-block rounded-pill m-3 w-100">Countinue with Google</button>
 
-
-
-                        <div className="form-group">
-                          <div className="input-group position-relative">
-                            <input type="text" className="form-control rounded-pill" placeholder="Phone number" id="phone"
-                              name="phone" pattern="01[0-2]{1}[0-9]{8}" required value={this.state.phone} onChange={this.handlePhoneChange}></input>
-                            <div className="invalid-feedback text-center">
-                              Please enter your Egyption phone like "01*********"
-                            </div>
 
                           </div>
                         </div>
-
-                        <div className="form-check mt-4">
-                          <input className="form-check-input" type="checkbox" id="send_emails" name="send_emails"></input>
-                          <label className="form-check-label fixedFonts" htmlFor="send_emails">
-                            Send me helpful emails to find rewarding work and job leads
-                          </label>
-                        </div>
-
-                        <div className=" row form-check mt-2">
-                          <div className="col-md-12 mb-4">
-                            <div className="form-outline position-relative">
-                              <input className="form-check-input" type="checkbox" id="agree_terms" name="agree_terms" required></input>
-                              <label className="form-check-label fixedFonts" htmlFor="agree_terms">
-                                Yes, I understand and agree to the Upwork Terms of Service, including the <a href="#" className="text-success">User
-                                  Agreement</a> and <a href="#" className="text-success">Privacy Policy</a>
-                              </label>
-                              <div className="invalid-feedback text-danger" id="email-feedback">
-                                Please accept the Upwork Terms of Service before continuing
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-
-
-                        <div className="mt-4 pt-2">
-                          <input className="btn btn-success btn-block rounded-pill m-3 w-100" type="submit" value="Submit" />
-                        </div>
-
-                        <div className="text-center">
-                          Already have an account?
-                          <NavLink to={'/login'} className="text-center text-success mt-3"> Login</NavLink>
-                        </div>
-                      </form>
-                      <div className="my-3">
-
-                        <div>
-                          <button className="btn btn-primary btn-block rounded-pill m-3 w-100">Countinue with Google</button>
-
-
-                        </div>
+                        <hr />
                       </div>
-                      <hr />
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </section>
+            </section>
+          </div>
+
+
+
         </div>
 
 
-
-      </div>
-
-
-    )
+      )
     }
   }
 
